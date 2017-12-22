ccd2b6f681ed0394c749c3d97fa9d7278dc2ed7e
Joseph Billingsley <jb@josephbillingsley.co.uk>
2017-12-22 16:44:26 +0000

    Improved order of running tests

--- run_tests.sh
+++ run_tests.sh
@@ -2,13 +2,13 @@
 
 command > tests.out 2>test.error
 
-for run_idx in {0..20} 
+for dim_idx in {0..4}
 do
-	for algo_idx in {0..7}  
+	for run_idx in {0..20}
 	do
-		for test_idx in {0..1}  
-		do
-			for dim_idx in {0..5}  
+		for test_idx in {0..7}  
+		do  	
+			for algo_idx in {0..7}
 			do
 				if [ $test_idx -lt 4 ] && [ $dim_idx -gt 0 ]
 				then
